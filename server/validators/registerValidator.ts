c826c5ed83bcdcb4d98d61f28e16577f3305858b
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-07-15 22:58:48 +0200

    solving some issues

--- server/validators/registerValidator.ts
+++ server/validators/registerValidator.ts
@@ -3,7 +3,7 @@ import Joi from "joi";
 const registerSchema = Joi.object({
   name: Joi.string().min(3).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
+  password: Joi.string().min(8).required(),
 });
 
 const registerValidator = (params: any) => {
