ed061f3a238997717ac010111115d8a461c407a7
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-07-18 00:42:26 +0200

    adding logout button

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -7,7 +7,15 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-800 ">
       {cookies.token ? (
-        <div className="text-5xl text-white ">You are logged in!</div>
+        <div className="flex flex-col items-center justify-center h-screen space-y-10 bg-gray-800 ">
+          <div className="text-5xl text-white ">You are logged in!</div>
+          <button
+            className="px-4 py-2 font-bold text-white bg-red-500 rounded hover:bg-red-700"
+            onClick={() => removeCookie("token")}
+          >
+            Logout
+          </button>
+        </div>
       ) : (
         <Login />
       )}
