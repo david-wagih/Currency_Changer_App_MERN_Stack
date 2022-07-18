a8ad2592d9a656b689d9fe770d766f3035937418
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-07-18 11:17:54 +0200

    finshing the task

--- client/src/components/FavoritesBag.tsx
+++ client/src/components/FavoritesBag.tsx
@@ -27,20 +27,23 @@ function FavoritesBag(props: any) {
       <h1 className="mb-4 text-2xl font-bold text-center text-white">
         My Favorites Bag
       </h1>
-      <div>
-        {favorites.map((favorite: any) => (
-          <div
-            key={favorite._id}
-            className="flex flex-col items-center justify-between"
-          >
-            <div className="flex flex-row items-center justify-center bg-green-800 ">
-              <p className="text-xl text-white ">
-                {favorite.baseCurrency}
-                {"  "} to {"   "} {favorite.targetCurrency}
-              </p>
+      <div className="flex flex-col items-center space-y-10 justify-evenly">
+        {
+          // @ts-ignore
+          favorites.map((favorite: any) => (
+            <div
+              key={favorite._id}
+              className="flex flex-col items-center justify-between"
+            >
+              <div className="flex flex-row items-center justify-center bg-green-800 ">
+                <p className="text-xl text-white ">
+                  {favorite.baseCurrency}
+                  {"  "} to {"   "} {favorite.targetCurrency}
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        }
       </div>
     </div>
   );
